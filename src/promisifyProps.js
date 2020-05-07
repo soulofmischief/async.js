72058672c3e3c80355d727fe3f385f50f506f914
soulofmischief <soulofmischief@protonmail.com>
2020-05-07 10:44:47 -0500

    Fix promisifyProps

--- src/promisifyProps.js
+++ src/promisifyProps.js
@@ -1,17 +1,18 @@
 // @flow strict
-import { isObject, isPromise, zip } from '@soulofmischief/js-utils'
+import { changeValues, isObject } from '@soulofmischief/js-utils'
+import { isPromise } from './isPromise'
+
 
 /**
  * Recursively wrap each property in a promise.
  */
 export function promisifyProps( o ) {
-  return zip(
-    Object.keys( o ),
-    Object.values( o ).map( v => {
-      if ( isObject( v ) && !isPromise( v ))
-        return promisifyProps( v )
 
-      else return Promise.resolve( v )
-    })
-  )
+  if ( Array.isArray( o ))
+    return o.map( x => promisifyProps( x ))
+
+  if ( isObject( o ) && !isPromise( o ))
+    return changeValues( o, v => promisifyProps( v ))
+
+  return Promise.resolve( o )
 }
--- tests/promisifyProps.js
+++ tests/promisifyProps.js
@@ -2,16 +2,26 @@ import o from 'ospec'
 import { promisifyProps, resolveProps } from '../src'
 
 
-const t = {
-  a: Promise.resolve( 1 ),
-  b: {
-    a: Promise.resolve( 2 ),
-    b: Promise.resolve( 3 ),
-  },
-}
+const
+  // Deep equals doesn't work on promises, so instead we wrap
+  // promisifyProps() in resolveProps() and compare two separate objects.
+  obj1 = { a: 1, b: { c: 2, d: { e: 3, f: 4, }, }, g: [ 4, 5, ]},
+  obj2 = {
+    a: Promise.resolve( 1 ),
+    b: {
+      c: Promise.resolve( 2 ),
+      d: {
+        e: Promise.resolve( 3 ),
+        f: Promise.resolve( 4 ),
+      },
+    },
+    g: [
+      Promise.resolve( 4 ),
+      Promise.resolve( 5 ),
+    ]
+  }
 
-
-o( 'promisifyProps', async function () {
-  o( promisifyProps( await resolveProps( t )))
-    .deepEquals( t )
+o( 'promisifyProps', async () => {
+  o( await resolveProps( promisifyProps( obj1 )))
+    .deepEquals( await resolveProps( obj2 ))
 })

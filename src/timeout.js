72f98b4c605463e1a72512a3eeffe6bba489be3a
soulofmischief <soulofmischief@protonmail.com>
2020-09-17 06:22:40 -0500

    Add option to pass a custom result

--- src/timeout.js
+++ src/timeout.js
@@ -1,6 +1,6 @@
 // @flow strict
 
 /** Wait for the specified amount of time. */
-export function timeout( ms: number ) {
-  return new Promise( resolve => setTimeout( resolve.bind( null, true ), ms ))
+export function timeout( ms: number, result = true ) {
+  return new Promise( resolve => setTimeout( resolve.bind( null, result ), ms ))
 }

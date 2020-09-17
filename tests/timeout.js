072f32b217a14a982c0ee5171534d42610875748
soulofmischief <soulofmischief@protonmail.com>
2020-09-17 06:22:58 -0500

    Add test for timeout()

---
+++ tests/timeout.js
@@ -0,0 +1,20 @@
+import o from 'ospec'
+import { timeout } from '../src'
+
+const time = 10
+
+o.spec( 'timeout', () => {
+  o( 'waits for correct amount of time', async () => {
+    let result = false
+    setTimeout(() => result = true, time )
+    await timeout( time ) && o( result ).equals( true )
+  })
+
+  o( 'returns true by default', async () => {
+    o( await timeout( time )).equals( true )
+  })
+
+  o( 'returns second parameter', async () => {
+    o( await timeout( time, 'test' )).equals( 'test' )
+  })
+})
